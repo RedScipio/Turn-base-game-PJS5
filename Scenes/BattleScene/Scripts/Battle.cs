273b6431a12d67bb5c03895cd40b96de6c136a14
RedScipio <bilbo2121@laposte.net>
2023-10-16 10:11:48 +0200

    Initial commit 3/3

---
+++ Scenes/BattleScene/Scripts/Battle.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class Battle : Control
+{
+	//Native from Godot
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	//Native from Godot
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
