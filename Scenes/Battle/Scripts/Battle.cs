f6347c58046b460a090a23f92b55c05dff4ab0bd
A2drien <adriendubois1411@gmail.com>
2023-10-17 20:22:06 +0200

    Modif de Battle.cs (ne compile pas -pas de dépendances-)

--- Scenes/Battle/Scripts/Battle.cs
+++ Scenes/Battle/Scripts/Battle.cs
@@ -1,17 +1,39 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+
+namespace Scene.Battle;
 
 public partial class Battle : Control
 {
+	private List<IPilot> _lPilotes;
+	
 	//Native from Godot
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		
+		this._lPilotes = lPilotes;
 	}
 
 	//Native from Godot
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public bool BattleIsOver()
+	{
+		return false;
+	}
+
+	/*private bool PlayTurn(PilotTurn* pPilotTurn)
+	{
+		return false;
+	}*/
+
+	private bool EnemyTurn()
+	{
+		return false;
 	}
 }
