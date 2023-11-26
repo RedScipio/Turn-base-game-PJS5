25fe869cae1c2539c0777c6415eebadb9fbb56af
Beowulf-0 <WahadaMehdi@gmail.com>
2023-11-26 19:13:21 +0100

    Ecriture d'un test retracant un tour de combat

---
+++ PJS5_CSharp/Test_PJS5_CSharp/GlobalUsings.cs
@@ -0,0 +1,7 @@
+global using Xunit;
+global using PJS5_CSharp.Sources.Pilot;
+global using PJS5_CSharp.Sources.Weapon.MeleeWeapon;
+global using PJS5_CSharp.Sources.Weapon.NormalWeapon;
+global using PJS5_CSharp.Sources.Weapon.ProjectileWeapon;
+global using PJS5_CSharp.Sources.Weapon.ThermalWeapon;
+global using PJS5_CSharp.Sources.Robot;
