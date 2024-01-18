fba660451d8ef55e95b5be6fb61cab0025002321
Beowulf-0 <73909294+Beowulf-0@users.noreply.github.com>
2024-01-18 23:18:59 +0100

    Delete PJS5_POC_Cpp directory

--- PJS5_POC_Cpp/Sources/Pilot/IPilot.cpp
+++
@@ -1,9 +0,0 @@
-#include "IPilot.h"
-
-namespace PILOT
-{
-	IPILOT::IPILOT(const ROBOT::ROBOT& robot)
-		: _pRobot(new ROBOT::ROBOT(robot))
-	{
-	}
-}
--- PJS5_POC_Cpp/main.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include <iostream>
-#include "Sources/Gui/Gui.h"
-
-int main()
-{
-    std::cout << "Hello World!\n";
-    GUI::MainMenu();
-}
