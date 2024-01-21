3a4486f92e5a16de3bc76ff8989b9901eaf568ac
SadnessCrystal <freewere_cycling@cmejat.com>
2024-01-21 23:15:04 +0100

    Finish Bots

---
+++ PJS5_CSharp/PJS5_CSharp/Sources/Pilot/Bot/DumbBotPilot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJS5_CSharp.Sources.Pilot.Bot
+{
+    internal class DumbBotPilot : BOT_PILOT
+    {
+        public DumbBotPilot(Robot.Robot pRobot, List<int> vFuelsReserve, List<int> vRepairKitsReserve) : base(pRobot, vFuelsReserve, vRepairKitsReserve)
+        {
+        }
+
+        public override void PlayTurn(Robot.Robot ennemiRobot)
+        {
+            if (this.GetRobot().WeaponIsUsable(Robot.WEAPON_SIDE.LEFT_WEAPON))
+            {
+                this.GetRobot().DealDamage(ennemiRobot, Robot.WEAPON_SIDE.LEFT_WEAPON, 2);
+            }
+
+            else if (this.GetRobot().WeaponIsUsable(Robot.WEAPON_SIDE.RIGHT_WEAPON))
+            {
+                this.GetRobot().DealDamage(ennemiRobot, Robot.WEAPON_SIDE.RIGHT_WEAPON, 2);
+            }
+        }
+    }
+}
