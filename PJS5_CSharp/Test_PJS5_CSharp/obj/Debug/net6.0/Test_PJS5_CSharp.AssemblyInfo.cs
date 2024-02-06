909dad56e9b3596a44ff839f4837bc85b39e9fea
SadnessCrystal <freewere_cycling@cmejat.com>
2024-02-06 11:03:19 +0100

    Resolve conflits

--- PJS5_CSharp/Test_PJS5_CSharp/obj/Debug/net6.0/Test_PJS5_CSharp.AssemblyInfo.cs
+++ PJS5_CSharp/Test_PJS5_CSharp/obj/Debug/net6.0/Test_PJS5_CSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test_PJS5_CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8036d35104356b2544af1f1fe0efbce8f8afcb78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test_PJS5_CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test_PJS5_CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
