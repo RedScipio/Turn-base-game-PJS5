c17edc4b8b0be403dbc5f60d479d4dc90bf9716b
Beowulf-0 <73909294+Beowulf-0@users.noreply.github.com>
2024-02-06 11:27:05 +0100

    added TestRobot.cs

--- PJS5_CSharp/Test_PJS5_CSharp/TestRobot.cs
+++ PJS5_CSharp/Test_PJS5_CSharp/TestRobot.cs
@@ -29,7 +29,7 @@ namespace Test_PJS5_CSharp
 
         List<int> playerFuelsReserve = new List<int> { 10, 10, 10, 10 };
         List<int> playerRepairKits = new List<int> { 10, 10, 10, 10 };
-
+        //les mettre
         List<int> botFuelsReserve = new List<int> { 10, 10, 10, 10 };
         List<int> botRepairKits = new List<int> { 10, 10, 10, 10 };
 
@@ -49,30 +49,35 @@ namespace Test_PJS5_CSharp
             robotBot = new Robot(furnBot, legsBot, leftBotWeap, rightBotWeap);
 
             pPlayerPilot = new PLAYER_PILOT(robotPlayer, playerFuelsReserve, playerRepairKits);
-            pBotPilot = FactoryBot.Build(robotBot, botFuelsReserve, botRepairKits, FactoryBot.TypeBot.Dumb);
+            pBotPilot = new BOT_PILOT(robotBot, botFuelsReserve, botRepairKits);
         }
         
         [Fact] 
         public void TestDealingDamageWithDifferentWeapons()
         {
-            int iEnemyActualLegsArmorValue = robotPlayer.DealDamage(robotBot, WEAPON_SIDE.LEFT_WEAPON, PARTS_TYPE.LEGS);
+            //Check if a weapon really inherit from IPARTS class and implement the IWEAPON interface
+            IWeapon weapon = new IWeapon(1, "Basic Nonworking Weapon", 3, 1, 4, 25, 60, 40, WEAPON_TYPE.ABSTRACT_WEAPON);
+            weapon.Should().BeAssignableTo<IPARTS>().And.BeAssignableTo<IWEAPON>();
 
+            int iEnemyActualLegsArmorValue = robotPlayer.DealDamage(robotBot, 1, 3);
             iEnemyActualLegsArmorValue.Should().Be(robotBot.GetLegsMaxArmor() - 2);
             Assert.Equal(robotBot.GetLegsArmor(), iEnemyActualLegsArmorValue);
 
+            //note : peut-être lever une exception
             //Inflicting damage on a non-existent robot part
-            //int iNonExistantPart = robotPlayer.DealDamage(robotBot, WEAPON_SIDE.RIGHT_WEAPON, 5);
-            //iNonExistantPart.Should().Be(0);
+            int iNonExistantPart = robotPlayer.DealDamage(robotBot, 2, 5);
+            iNonExistantPart.Should().Be(0);
 
             //Inflicting damage on a robot with a thermal weapon and checking if fuel has been removed from the enemy
-            robotPlayer.SetWeapon(WEAPON_SIDE.RIGHT_WEAPON, new THERMAL_WEAPON(1, "Basic Thermal Weapon", 3, 1, 1, 25, 60, 40, 25));
+            robotPlayer.SetWeapon(2, new THERMAL_WEAPON(1, "Basic Thermal Weapon", 3, 1, 1, 25, 60, 40, 25));
+            //pull 
             robotPlayer.GetRightWeaponType().Should().Be(4);
             robotPlayer.GetRightWeaponSpecificity().Should().Be(25);
             pPlayerPilot.PlayTurn(robotBot, 1, 2, 1, 1);
             robotBot.GetFuel().Should().Be(75);
 
             //Dealing damage to a robot with a projectile weapon and checking if ammo has been removed from it.
-            robotPlayer.SetWeapon(WEAPON_SIDE.RIGHT_WEAPON, new PROJECTILE_WEAPON(1, "Basic Projectile Weapon", 3, 1, 4, 25, 60, 40, 5));
+            robotPlayer.SetWeapon(2, new PROJECTILE_WEAPON(1, "Basic Projectile Weapon", 3, 1, 4, 25, 60, 40, 5));
             robotPlayer.GetRightWeaponType().Should().Be(3);
             robotPlayer.GetRightWeaponSpecificity().Should().Be(5);   
             pPlayerPilot.PlayTurn(robotBot, 1, 2, 1, 50);
@@ -82,33 +87,58 @@ namespace Test_PJS5_CSharp
         [Fact]
         public void TestDifferentsWeaponIsUsable()
         {
+            IWeapon pNonWorkingWeapon = new IWeapon(1, "Basic Nonworking Weapon", 3, 1, 4, 25, 60, 40, WEAPON_TYPE.ABSTRACT_WEAPON);
             IWeapon pProjectileWeapon = new PROJECTILE_WEAPON(1, "Basic Projectile Weapon", 3, 1, 1, 25, 60, 50, 1);
             IWeapon pMeleeWeapon = new MELEE_WEAPON(1, "Basic Melee Weapon", 3, 1, 1, 25, 100, 0);
             IWeapon pNormalWeapon = new NORMAL_WEAPON(1, "Basic Normal Weapon", 3, 1, 1, 100, 80, 40);
 
+            //Checking if an abstract weapon is not usable
+            robotPlayer.SetWeapon(1, pNonWorkingWeapon);
+            robotPlayer.WeaponIsUsable(1).Should().BeFalse();
+
             //Projectile weapon testing
-            robotPlayer.SetWeapon(WEAPON_SIDE.LEFT_WEAPON, pProjectileWeapon);
-            robotPlayer.WeaponIsUsable(WEAPON_SIDE.LEFT_WEAPON).Should().BeTrue();
+            robotPlayer.SetWeapon(1, pProjectileWeapon);
+            robotPlayer.WeaponIsUsable(1).Should().BeTrue();
             //Checking if an empty weapon isn't usable.
             pPlayerPilot.PlayTurn(robotBot, 1, 1, 1, 1);
-            robotPlayer.WeaponIsUsable(WEAPON_SIDE.LEFT_WEAPON).Should().BeFalse();
+            robotPlayer.WeaponIsUsable(1).Should().BeFalse();
 
             //Melee weapon testing
-            robotPlayer.SetWeapon(WEAPON_SIDE.LEFT_WEAPON, pMeleeWeapon);
-            for (int i = 0; i <= 1; i++) robotBot.DealDamage(robotPlayer, WEAPON_SIDE.LEFT_WEAPON, PARTS_TYPE.LEGS);
-            robotPlayer.WeaponIsUsable(WEAPON_SIDE.LEFT_WEAPON).Should().BeFalse();
+            robotPlayer.SetWeapon(1, pMeleeWeapon);
+            for (int i = 0; i <= 1; i++) robotBot.DealDamage(robotPlayer, 1, 3);
+            robotPlayer.WeaponIsUsable(1).Should().BeFalse();
 
             //Normal weapon testing
-            robotPlayer.SetWeapon(WEAPON_SIDE.LEFT_WEAPON, pNormalWeapon);
+            robotPlayer.SetWeapon(1, pNormalWeapon);
             pPlayerPilot.PlayTurn(robotBot, 1, 1, 1);
-            robotPlayer.WeaponIsUsable(WEAPON_SIDE.LEFT_WEAPON).Should().BeFalse();
+            robotPlayer.WeaponIsUsable(1).Should().BeFalse();
         }
 
         [Fact]
-        public void TestWeaponHitChance()
+        public void TestDifferentWeaponsHitChance()
         {
+            IWeapon pProjectileWeapon = new PROJECTILE_WEAPON(1, "Basic Projectile Weapon", 3, 1, 1, 25, 60, 50, 1);
+            IWeapon pMeleeWeapon = new MELEE_WEAPON(1, "Basic Melee Weapon", 3, 1, 1, 25, 100, 0);
+            IWeapon pNormalWeapon = new NORMAL_WEAPON(1, "Basic Normal Weapon", 3, 1, 1, 100, 80, 40);
             //Checking the calculation of the hit chance
+
+            //Checking Melee Weapon Hit chance
+            robotPlayer.SetWeapon(1, pMeleeWeapon);
+            robotPlayer.GetLeftWeaponHitChance().Should().Be(100);
+
+            robotBot.DealDamage(robotPlayer, 1, 3);
+            robotPlayer.GetLeftWeaponHitChance().Should().Be(50);
+
+            //Repairing legs and checking hit chance on another weapon
+            pPlayerPilot.PlayTurn(robotPlayer, 2, 3, 3);
+            robotPlayer.SetWeapon(1, pNormalWeapon);
+            robotPlayer.GetLeftWeaponHitChance().Should().Be(80);
+
+            //Switching to projectile weapon and after one leg broken 
+            robotPlayer.SetWeapon(1,pProjectileWeapon);
+            robotBot.DealDamage(robotPlayer, 1, 3);
+            robotPlayer.GetLeftWeaponHitChance().Should().Be(55);
         }
     }
 }
-
+//déterminer ce qui présente la classe prenant le plus de risques et 
