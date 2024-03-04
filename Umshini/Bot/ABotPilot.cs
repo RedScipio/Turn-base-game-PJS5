a97027e9c5d6539831fe5e28c87df378548fd19c
SadnessCrystal <52292872+SadnessCrystal@users.noreply.github.com>
2024-03-04 22:38:24 +0100

    PlayRound & PlayTurn rework 2/?

---
+++ Umshini/Bot/ABotPilot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Battle;
+using Pilot;
+
+namespace Bot
+{
+    public class ABotPilot : APILOT
+    {
+        public ABotPilot(IROBOT pRobot, List<int> vFuelsReserve, List<int> vRepairKitsReserve) : base(pRobot, vFuelsReserve, vRepairKitsReserve)
+        {
+        }
+    }
+}
