03020121bf98183c9bfaa3a98ef70d7e23159854
Beowulf-0 <73909294+Beowulf-0@users.noreply.github.com>
2024-05-06 15:39:08 +0200

    wip TestAPilot.cs

---
+++ Umshini/Umshini_Test/TestAPilot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umshini_Test
+{
+    public class TestAPilot
+    {
+        /// <summary>
+        /// Tester les méthodes Attack, Repair et Refuel de la classe APilot
+        /// </summary>
+        public TestAPilot() 
+        {
+            
+        }   
+    }
+}
