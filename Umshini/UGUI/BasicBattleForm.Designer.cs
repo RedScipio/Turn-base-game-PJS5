d310b4af8ef748c6dff9738545dd5c95b5d4b677
chiefnodus <nodusp.contact@gmail.com>
2024-06-26 15:34:09 +0200

    Fix for HRESULT E_FAIL

--- Umshini/UGUI/BasicBattleForm.Designer.cs
+++ Umshini/UGUI/BasicBattleForm.Designer.cs
@@ -48,7 +48,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1090, 551);
             this.Controls.Add(this.lever1);
             this.DoubleBuffered = true;
             this.Name = "BasicBattleForm";
