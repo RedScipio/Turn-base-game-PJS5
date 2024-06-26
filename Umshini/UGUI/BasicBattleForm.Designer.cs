d8ad84c871fcc2ba318874622b6a66973c8b0068
chiefnodus <nodusp.contact@gmail.com>
2024-06-26 16:01:33 +0200

    Added responsive score board

--- Umshini/UGUI/BasicBattleForm.Designer.cs
+++ Umshini/UGUI/BasicBattleForm.Designer.cs
@@ -32,12 +32,13 @@
             this.generalLayout = new System.Windows.Forms.TableLayoutPanel();
             this.BarMenuPictureBox = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.ScoreLayout = new System.Windows.Forms.TableLayoutPanel();
-            this.scoreStatus2 = new Battle.ScoreStatus();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.scoreStatus1 = new Battle.ScoreStatus();
+            this.scoreStatus3 = new Battle.ScoreStatus();
             this.generalLayout.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.BarMenuPictureBox)).BeginInit();
             this.tableLayoutPanel2.SuspendLayout();
-            this.ScoreLayout.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // generalLayout
@@ -73,49 +74,67 @@
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel2.Controls.Add(this.ScoreLayout, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 1, 1);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 2;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
             this.tableLayoutPanel2.Size = new System.Drawing.Size(1084, 379);
             this.tableLayoutPanel2.TabIndex = 1;
             // 
-            // ScoreLayout
+            // tableLayoutPanel1
             // 
-            this.ScoreLayout.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ScoreLayout.BackgroundImage")));
-            this.ScoreLayout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ScoreLayout.ColumnCount = 5;
-            this.ScoreLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.ScoreLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.ScoreLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.ScoreLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.ScoreLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.ScoreLayout.Controls.Add(this.scoreStatus2, 1, 0);
-            this.ScoreLayout.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ScoreLayout.Location = new System.Drawing.Point(371, 10);
-            this.ScoreLayout.Margin = new System.Windows.Forms.Padding(10);
-            this.ScoreLayout.Name = "ScoreLayout";
-            this.ScoreLayout.RowCount = 2;
-            this.ScoreLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
-            this.ScoreLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
-            this.ScoreLayout.Size = new System.Drawing.Size(341, 169);
-            this.ScoreLayout.TabIndex = 0;
+            this.tableLayoutPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tableLayoutPanel1.BackgroundImage")));
+            this.tableLayoutPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.41667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 31.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 31.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.41667F));
+            this.tableLayoutPanel1.Controls.Add(this.scoreStatus1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.scoreStatus3, 3, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(371, 47);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(10);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(341, 207);
+            this.tableLayoutPanel1.TabIndex = 2;
             // 
-            // scoreStatus2
+            // scoreStatus1
             // 
-            this.scoreStatus2.BackColor = System.Drawing.Color.Transparent;
-            this.scoreStatus2.FurnaceColor = System.Drawing.Color.Gray;
-            this.scoreStatus2.LeftArmColor = System.Drawing.Color.Gray;
-            this.scoreStatus2.LegsColor = System.Drawing.Color.Gray;
-            this.scoreStatus2.Location = new System.Drawing.Point(180, 10);
-            this.scoreStatus2.Margin = new System.Windows.Forms.Padding(10);
-            this.scoreStatus2.Name = "scoreStatus2";
-            this.scoreStatus2.RightArmColor = System.Drawing.Color.Gray;
-            this.scoreStatus2.Size = new System.Drawing.Size(63, 45);
-            this.scoreStatus2.TabIndex = 1;
+            this.scoreStatus1.BackColor = System.Drawing.Color.Transparent;
+            this.scoreStatus1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scoreStatus1.FurnaceColor = System.Drawing.Color.Gray;
+            this.scoreStatus1.LeftArmColor = System.Drawing.Color.Gray;
+            this.scoreStatus1.LegsColor = System.Drawing.Color.Gray;
+            this.scoreStatus1.Location = new System.Drawing.Point(45, 30);
+            this.scoreStatus1.Margin = new System.Windows.Forms.Padding(10);
+            this.scoreStatus1.Name = "scoreStatus1";
+            this.scoreStatus1.RightArmColor = System.Drawing.Color.Gray;
+            this.scoreStatus1.Size = new System.Drawing.Size(86, 83);
+            this.scoreStatus1.TabIndex = 0;
+            // 
+            // scoreStatus3
+            // 
+            this.scoreStatus3.BackColor = System.Drawing.Color.Transparent;
+            this.scoreStatus3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scoreStatus3.FurnaceColor = System.Drawing.Color.Gray;
+            this.scoreStatus3.LeftArmColor = System.Drawing.Color.Gray;
+            this.scoreStatus3.LegsColor = System.Drawing.Color.Gray;
+            this.scoreStatus3.Location = new System.Drawing.Point(207, 30);
+            this.scoreStatus3.Margin = new System.Windows.Forms.Padding(10);
+            this.scoreStatus3.Name = "scoreStatus3";
+            this.scoreStatus3.RightArmColor = System.Drawing.Color.Gray;
+            this.scoreStatus3.Size = new System.Drawing.Size(86, 83);
+            this.scoreStatus3.TabIndex = 1;
             // 
             // BasicBattleForm
             // 
@@ -132,7 +151,7 @@
             this.generalLayout.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.BarMenuPictureBox)).EndInit();
             this.tableLayoutPanel2.ResumeLayout(false);
-            this.ScoreLayout.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -142,7 +161,8 @@
         private System.Windows.Forms.TableLayoutPanel generalLayout;
         private System.Windows.Forms.PictureBox BarMenuPictureBox;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-        private System.Windows.Forms.TableLayoutPanel ScoreLayout;
-        private Battle.ScoreStatus scoreStatus2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private Battle.ScoreStatus scoreStatus1;
+        private Battle.ScoreStatus scoreStatus3;
     }
 }
