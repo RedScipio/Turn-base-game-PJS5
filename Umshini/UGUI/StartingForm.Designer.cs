2a5c1c1433191a5e9aca28e69fb27fde0e03431a
chiefnodus <nodusp.contact@gmail.com>
2024-06-26 23:03:26 +0200

    Choosing menu enhanced

--- Umshini/UGUI/StartingForm.Designer.cs
+++ Umshini/UGUI/StartingForm.Designer.cs
@@ -29,62 +29,67 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.customButton1 = new UGUI.CustomButton();
+            this.customButton2 = new UGUI.CustomButton();
+            this.customButton3 = new UGUI.CustomButton();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Agency FB", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(477, 49);
+            this.label1.Font = new System.Drawing.Font("Agency FB", 50F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(422, 34);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 41);
+            this.label1.Size = new System.Drawing.Size(214, 80);
             this.label1.TabIndex = 1;
             this.label1.Text = "Umshini";
             // 
-            // button3
+            // customButton1
             // 
-            this.button3.Location = new System.Drawing.Point(343, 196);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(389, 47);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Choosing equipment";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.customButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(218)))), ((int)(((byte)(210)))));
+            this.customButton1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(218)))), ((int)(((byte)(210)))));
+            this.customButton1.LabelText = "Start game";
+            this.customButton1.Location = new System.Drawing.Point(343, 143);
+            this.customButton1.Name = "customButton1";
+            this.customButton1.Size = new System.Drawing.Size(389, 47);
+            this.customButton1.TabIndex = 7;
+            this.customButton1.Click += new System.EventHandler(this.customButton1_Click);
             // 
-            // button2
+            // customButton2
             // 
-            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.button2.Location = new System.Drawing.Point(343, 249);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(389, 47);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Quit game";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.customButton2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(218)))), ((int)(((byte)(210)))));
+            this.customButton2.Color = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(218)))), ((int)(((byte)(210)))));
+            this.customButton2.LabelText = "Choosing equipment";
+            this.customButton2.Location = new System.Drawing.Point(343, 196);
+            this.customButton2.Name = "customButton2";
+            this.customButton2.Size = new System.Drawing.Size(389, 47);
+            this.customButton2.TabIndex = 8;
+            this.customButton2.Click += new System.EventHandler(this.customButton2_Click);
             // 
-            // button1
+            // customButton3
             // 
-            this.button1.Location = new System.Drawing.Point(343, 143);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(389, 47);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Start game";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.customButton3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.customButton3.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.customButton3.LabelText = "Quit game";
+            this.customButton3.Location = new System.Drawing.Point(343, 249);
+            this.customButton3.Name = "customButton3";
+            this.customButton3.Size = new System.Drawing.Size(389, 47);
+            this.customButton3.TabIndex = 9;
+            this.customButton3.Click += new System.EventHandler(this.customButton3_Click);
             // 
             // StartingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
             this.ClientSize = new System.Drawing.Size(1090, 551);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.customButton3);
+            this.Controls.Add(this.customButton2);
+            this.Controls.Add(this.customButton1);
             this.Controls.Add(this.label1);
             this.Name = "StartingForm";
+            this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Umshini";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.StartingForm_FormClosing);
@@ -97,8 +102,8 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private CustomButton customButton1;
+        private CustomButton customButton2;
+        private CustomButton customButton3;
     }
 }
